a896536240d552387c1fba2d1a3d44c4408e8fb3
Duwab <admin@duwab.com>
2015-02-07 17:23:05 +0100

    cleaning, provides getData and lightbox

--- assets/js/artbeat.js
+++ assets/js/artbeat.js
@@ -5,19 +5,76 @@
 		emitNav : function(from, to, info){console.log('emitNav ws', from, to, info);}
 	};
 	
-	var ProvFunc = function(){
-		var version = 1;
-		return {
-			$get : function(){
-				return {version:version, title: 'Artbeat Angular'};
-			},
-			setVersion : function(newValue){
-				version = newValue;
+	
+	//	--- Test providers, services, factories		=>		http://blog.xebia.com/2013/09/01/differences-between-providers-in-angularjs/
+	var MyFunc = function() {
+		this.name = "default name";
+		this.$get = function() {
+			return "MyFunc.$get(). this.name = " + this.name;
+		};
+		this.setVersion = function(newValue){
+			this.name = newValue;
+		}
+		return "MyFunc(). this.name = " + this.name;
+	};
+
+	app.provider( 'myProv', MyFunc );	//injecté, myProv sera le résultat du $get
+
+	app.service( 'myService', MyFunc );	//injecté, myService sera MyFunc
+
+	app.factory( 'myFactory', MyFunc );	//injecté, myFactory sera le résultat de la fonction (le return)
+	//  --- End test providers
+
+	
+	app.factory('getData', function($http) {
+		var promise; // cache
+		var myService = {
+			async: function(url) {
+				if ( !promise ) {
+					promise = $http.get(url).then(function (response) {
+						if(response.data && response.data.status == "Success")
+							return response.data.data;
+						else
+							throw response.data;
+					}, function(err){	// vérifier l'utilité
+						throw err;
+					});
+				}
+				return promise;
 			}
 		};
-	}
-
-	app.provider( 'myProv', ProvFunc );  //injecté, myProv sera le résultat du get
+		return myService;
+	});
+	
+	app.factory('lightbox', function(getData) {
+		return {
+			build : function(type, template, options){
+				console.log('build lightbox', getData, type, template);
+				options = options || {};
+				var lbx = $('<lightbox></lightbox>');
+				$('body').append(artbeat.compile(lbx)(artbeat.scope));
+				artbeat.scope.typeLbx = type;
+				artbeat.scope.$apply();
+				artbeat.addLightbox(lbx);
+				lbx.on('click', function(){
+					if($('.lbx_frame_content:hover', lbx).length == 0)
+					{
+						lbx.remove();
+						artbeat.delLightbox(lbx);
+					}
+				});
+				lbx.on('validate', function(){
+					lbx.remove();
+					artbeat.delLightbox(lbx);
+				});
+				lbx.on('cancel', function(){
+					alert('You suck!');
+					lbx.remove();
+					artbeat.delLightbox(lbx);
+				});
+			}
+		}
+	});
 	
 	app.config(function($routeProvider, $locationProvider, myProvProvider) {
 		$locationProvider.html5Mode({
@@ -25,46 +82,26 @@
 			requireBase: false
 		});
 		myProvProvider.setVersion(2);
-		/* $routeProvider
-		.when('/profile', { template: 'page1.html', controller: 'Page1Ctrl' })
-		.when('/page2', { template: 'page2.html', controller: 'Page2Ctrl' })
-		.otherwise({
-		  redirectTo: '/'
-		}); */
+		/*
+		$routeProvider
+			.when('/profile', { template: 'page1.html', controller: 'Page1Ctrl' })
+			.when('/page2', { template: 'page2.html', controller: 'Page2Ctrl' })
+			.otherwise({
+			  redirectTo: '/'
+			});
+		*/
 	});
 	
-	
-	/* 
-		Test providers, services, factories
-	*/
-	
-	var MyFunc = function() {
-
-		this.name = "default name";
-
-		this.$get = function() {
-			this.name = "new name"
-			return "Hello from MyFunc.$get(). this.name = " + this.name;
-		};
-
-		return "Hello from MyFunc(). this.name = " + this.name;
-	};
-	
-	//http://blog.xebia.com/2013/09/01/differences-between-providers-in-angularjs/
-
-	app.service( 'myService', MyFunc ); //injecté, myService sera MyFunc
-
-	app.factory( 'myFactory', MyFunc ); //injecté, myFactory sera le résultat de la fonction (le return)
-	
-	app.controller('ArtbeatController', ['$http', '$location', '$scope', '$compile', 'myService', 'myFactory', 'myProv', function($http, $location, $scope, $compile, myService, myFactory, myProv){
+	app.controller('ArtbeatController', ['$http', '$location', '$scope', '$compile', function($http, $location, $scope, $compile){
 		artbeat = this;
 		artbeat.lightbox = {count: 0};
 		artbeat.scope = $scope;
 		artbeat.compile = $compile;
 		
+		/* Pour le test en incluant ces providers dans la liste à l'initialisation du controller
 		$scope.serviceOutput = myService;
 		$scope.factoryOutput = myFactory;
-		$scope.providerOutput = myProv;
+		$scope.providerOutput = myProv; */
 		
 		artbeat.refreshLocation = function(){
 			artbeat.page = urlParse(location.pathname)[0];
@@ -73,14 +110,12 @@
 		
 		artbeat.user = false;
 		$http.get('/json/profile.json').success(function(data){
-			console.log('premier');
 			if(data.status == "Success")
 				artbeat.user = data.data;
 		});
 		$http.get('/json/post.json').success(function(data){
 			if(data.status == "Success")
 				$scope.post = data.data[0];
-			console.log($scope.post);
 		});
 		$http.get('/json/artwork.json').success(function(data){
 			if(data.status == "Success")
@@ -93,14 +128,6 @@
 		
 		$scope.logout = function(page){
 			artbeat.user = false;
-		}
-		
-		$scope.test_lightbox = function(page){
-			lightbox();
-		}
-		
-		$scope.test_popup = function(page){
-			popup();
 		}
 		
 		$scope.$watch(function () { return $location.url(); }, function (url) {
@@ -124,6 +151,22 @@
 
 	}]);
 	
+	app.controller('PageController', ['$http', '$location', '$scope', '$compile', function($http, $location, $scope, $compile){
+		// page controller, certainly not necessary
+	}]);
+	
+	app.controller('ProfileController', ['$http', '$location', '$scope', '$compile', function($http, $location, $scope, $compile){
+		// specific controller for profile page...
+		// to load only if necessary, and if you currently haven't loaded this controller, just open another page (end of SPA) or try to use requireJS
+	}]);
+	
+	
+	/*
+	 *
+	 *	--> Navigation -->
+	 *
+	*/
+	
 	app.directive('a', function() {
 		return {
 			restrict: 'E',
@@ -146,74 +189,6 @@
 	   };
 	});
 	
-	app.directive('popup', function() {
-		return {
-			restrict: 'A',
-			link: function(scope, elem, attrs) {
-				elem.on('click', function(e){
-					console.log('click popup');
-					var lbx = $('<lightbox></lightbox>');
-					$('body').append(artbeat.compile(lbx)(scope));
-					scope.$apply();
-					artbeat.addLightbox(lbx);
-					lbx.on('validate', function(){
-						// alert('validate');
-						lbx.remove();
-						artbeat.delLightbox(lbx);
-					});
-					lbx.on('cancel', function(){
-						alert('You suck!');
-						lbx.remove();
-						artbeat.delLightbox(lbx);
-					});
-					console.log(lbx);
-				});
-			}
-		}
-	});
-	
-	app.directive('popupValidate', function() {
-		return {
-			restrict: 'A',
-			link: function(scope, elem, attrs) {
-				elem.on('click', function(e){
-					elem.closest('lightbox').trigger('validate');
-				});
-			}
-		}
-	});
-	
-	app.directive('popupCancel', function() {
-		return {
-			restrict: 'A',
-			link: function(scope, elem, attrs) {
-				elem.on('click', function(e){
-					elem.closest('lightbox').trigger('cancel');
-				});
-			}
-		}
-	});
-	
-	app.directive('lightboxArtwork', function() {
-		return {
-			restrict: 'A',
-			link: function(scope, elem, attrs) {
-				elem.on('click', function(e){
-					alert('lightbox-artwork');
-				});
-			}
-		}
-	});
-	
-	app.controller('PageController', ['$http', '$location', '$scope', '$compile', function($http, $location, $scope, $compile){
-		/*$http.get('/json/profile.json').success(function(data){
-			console.log('deuxieme');
-			if(data.status == "Success")
-				artbeat.user = false;
-		});*/
-	}]);
-	
-	
 	function urlParse(url){
 		if(typeof url == "string" && ["","/"].indexOf(url.replace('','')) == -1)
 		{
@@ -228,18 +203,6 @@
 	window.onpopstate = function(event){
 		console.log('popstate');
 		artbeat.refreshLocation();
-	}
-	
-	
-	
-	function lightbox(tpl, item, options)
-	{
-		alert('lightbox');
-	}
-	
-	function popup(tpl, options)
-	{
-		alert('popup');
 	}
 	
 })();
--- assets/js/directives.js
+++ assets/js/directives.js
@@ -34,6 +34,21 @@
 		};
     });
 	
+	app.directive("suggestFriends", ['getData', function(getData) {
+		return {
+			restrict: "E",
+			templateUrl: "/assets/html/entity/suggest/friends.html",
+			controller:function($scope){
+				$scope.sgg_profiles = [];
+				getData.async('/json/suggest/friends.json').then(function(response){
+					$scope.sgg_profiles = response;
+				},function(err){
+					console.log('error !', err);
+				});
+			}
+		};
+    }]);
+	
 	app.directive("lightbox", function() {
 		return {
 			restrict: "E",
@@ -48,11 +63,75 @@
 		};
     });
 	
+	app.directive("fullscreenArtwork", function() {
+		return {
+			restrict: "E",
+			templateUrl: "/assets/html/lightbox/fullscreen/artwork.html"
+		};
+    });
+	
 	app.directive("alert", function() {
 		return {
 			restrict: "E",
 			templateUrl: "/assets/html/lightbox/popup/alert/test.html"
 		};
     });
+	
+	
+	
+	app.directive('popup', ['lightbox', function(lightbox) {
+		return {
+			restrict: 'A',
+			link: function(scope, elem, attrs) {
+				elem.on('click', function(e){
+					lightbox.build("popup");
+				});
+			}
+		}
+	}]);
+	
+	app.directive('lightbox', function(lightbox) {
+		return {
+			restrict: 'A',
+			link: function(scope, elem, attrs) {
+				elem.on('click', function(e){
+					lightbox.build("default");
+				});
+			}
+		}
+	});
+	
+	app.directive('popupValidate', function() {
+		return {
+			restrict: 'A',
+			link: function(scope, elem, attrs) {
+				elem.on('click', function(e){
+					elem.closest('lightbox').trigger('validate');
+				});
+			}
+		}
+	});
+	
+	app.directive('popupCancel', function() {
+		return {
+			restrict: 'A',
+			link: function(scope, elem, attrs) {
+				elem.on('click', function(e){
+					elem.closest('lightbox').trigger('cancel');
+				});
+			}
+		}
+	});
+	
+	app.directive('lightboxArtwork', function() {
+		return {
+			restrict: 'A',
+			link: function(scope, elem, attrs) {
+				elem.on('click', function(e){
+					alert('lightbox-artwork');
+				});
+			}
+		}
+	});
     
   })();
